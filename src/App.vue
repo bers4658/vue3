af619d08e398aeff25dac762e46d9d88435424a4
gorbunov4658 <gorbunov4658@yandex.ru>
2022-09-06 16:31:23 +0300

    Отрисовка по условию

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="app">
+        <post-form
+            @create="createPost"
+        />
+        <post-list
+            :posts="posts"
+            @remove="removePost"
+        />
+    </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm.vue"
+import PostList from "@/components/PostList.vue"
+export default {
+    components: {
+    PostForm,
+    PostList,
+},
+    data() {
+        return {
+            posts: [
+                {id: 1, title: 'JavaScript 1', body: 'Описание 1'},
+                {id: 2, title: 'JavaScript 2', body: 'Описание 2'},
+                {id: 3, title: 'JavaScript 3', body: 'Описание 3'},
+                {id: 4, title: 'JavaScript 4', body: 'Описание 4'},
+            ],
+        }
+    },
+
+    methods: {
+        createPost(post) {
+            this.posts.push(post);
+        },
+
+        removePost(post) {
+            this.posts = this.posts.filter(p => p.id !== post.id)
+        }
+
+       
+    }
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.app {
+    padding: 20px;
+}
+
+
+
+</style>
